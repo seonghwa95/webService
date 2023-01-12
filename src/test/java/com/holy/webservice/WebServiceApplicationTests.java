26bb48756a6792b644cf8975f86a5819b4af7866
holyflower95 <holyflower95@naver.com>
2023-01-12 09:03:48 +0900

    Initial commit

---
+++ src/test/java/com/holy/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holy.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
